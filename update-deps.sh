e99e3080ab244f22606db0fd6edfd29cc74b811a
Rui Craveiro <ruicraveiro@squarealfa.com>
2021-03-16 16:36:43 +0000

    Initial

---
+++ update-deps.sh
@@ -0,0 +1,55 @@
+cd ./generators/generators_common/ 
+dart pub get
+cd ../../
+
+cd ./generators/map_mapper
+dart pub get
+cd ../../
+
+cd ./generators/map_map_generator
+dart pub get
+cd ../../
+
+cd ./generators/proto_annotations
+dart pub get
+cd ../../
+
+cd ./generators/proto_generator
+dart pub get
+cd ../../
+
+cd ./generators/validation
+dart pub get
+cd ../../
+
+cd ./generators/validation
+dart pub get
+cd ../../
+
+cd ./generators/validation_generator
+dart pub get
+cd ../../
+
+cd ./generators/entity_adapter
+dart pub get
+cd ../../
+
+cd ./generators/entity_adapter_generator
+dart pub get
+cd ../../
+
+cd ./security
+dart pub get
+cd ../
+
+cd ./nosql_repository
+dart pub get
+cd ../
+
+
+cd ./arango_repository
+dart pub get
+cd ../
+
+
+ 
