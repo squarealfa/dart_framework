a969259d1b03b5a4ac00e8be56f55240ea5a5fad
Rui Craveiro <ruicraveiro@squarealfa.com>
2021-03-21 17:42:20 +0000

    Added proto_mapper_example

---
+++ proto_mapper/proto_mapper_example/example/proto_mapper_example_example.dart
@@ -0,0 +1,6 @@
+import 'package:proto_mapper_example/proto_mapper_example.dart';
+
+void main() {
+  var awesome = Awesome();
+  print('awesome: ${awesome.isAwesome}');
+}
---
+++ proto_mapper/proto_mapper_example/lib/proto_mapper_example.dart
@@ -0,0 +1,8 @@
+/// Support for doing something awesome.
+///
+/// More dartdocs go here.
+library proto_mapper_example;
+
+export 'src/proto_mapper_example_base.dart';
+
+// TODO: Export any libraries intended for clients of this package.
