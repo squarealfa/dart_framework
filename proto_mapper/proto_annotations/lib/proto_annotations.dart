8f9e4cc460bc89cddbda433f6c0e696175d3d299
Rui Craveiro <ruicraveiro@squarealfa.com>
2021-03-21 20:36:18 +0000

    Checkpoint

--- proto_mapper/proto_annotations/lib/proto_annotations.dart
+++ proto_mapper/proto_annotations/lib/proto_annotations.dart
@@ -1,8 +1,8 @@
-/// Support for doing something
-///
-/// More dartdocs go here.
+/// Contains annotations that mark classes for the generation of .proto files
+/// and dart mapper files
 library proto_mapper_annotations;
+
+export 'package:fixnum/fixnum.dart';
 
 export 'src/annotations.dart';
 export 'src/proto_mapper.dart';
-export 'package:fixnum/fixnum.dart';
--- proto_mapper/proto_annotations/lib/src/annotations.dart
+++ proto_mapper/proto_annotations/lib/src/annotations.dart
@@ -1,6 +1,6 @@
-export 'annotations/proto_field.dart';
-export 'annotations/proto_ignore.dart';
 export 'annotations/map_proto.dart';
 export 'annotations/map_proto_base.dart';
-export 'annotations/proto_base.dart';
 export 'annotations/proto.dart';
+export 'annotations/proto_base.dart';
+export 'annotations/proto_field.dart';
+export 'annotations/proto_ignore.dart';
