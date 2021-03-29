dcab76ed12b4605d48c5fbc9545d191ef3657aac
Rui Craveiro <ruicraveiro@squarealfa.com>
2021-03-29 15:53:22 +0100

    Refactored update-deps.sh to something way more elegant

--- update-deps.sh
+++ update-deps.sh
@@ -1,85 +1,63 @@
-cd ./generators_common
-dart pub get
-cd ../
+#!/bin/bash
 
-cd ./proto_mapper/proto_annotations/ 
-dart pub get
-cd ../../
-
-cd ./proto_mapper/proto_generator/ 
-dart pub get
-cd ../../
-
-cd ./proto_mapper/proto_mapper_example/ 
-dart pub get
-cd ../../
-
-cd ./proto_mapper/test/ 
-dart pub get
-cd ../../
-
-cd ./map_mapper/map_mapper_annotations
-dart pub get
-cd ../../
-
-cd ./map_mapper/map_mapper_generator
-dart pub get
-cd ../../
-
-cd ./map_mapper/map_mapper_example
-dart pub get
-cd ../../
-
-cd ./map_mapper/test
-dart pub get
-cd ../../
+paths=(
+'./generators_common'
+'./proto_mapper/proto_annotations/ '
+'./proto_mapper/proto_generator/ '
+'./proto_mapper/proto_mapper_example/ '
+'./proto_mapper/test/ '
+'./map_mapper/map_mapper_annotations'
+'./map_mapper/map_mapper_generator'
+'./map_mapper/map_mapper_example'
+'./map_mapper/test'
+'./defaults_provider/defaults_provider_annotations'
+'./defaults_provider/defaults_provider_generator'
+'./defaults_provider/defaults_provider_example'
+'./defaults_provider/test'
+'./entity/entity_annotations'
+'./entity/entity_generator'
+'./entity/example'
+'./entity/test'
+'./entity_adapter/entity_adapter'
+'./entity_adapter/entity_adapter_generator'
+'./entity_adapter/example'
+'./example_model'
+'./arango_driver'
+'./security'
+'./nosql_repository'
+'./arango_repository'
+)
 
 
+if [ -z $1 ]
+then
+    arg=""
+else
+    arg=$1
+fi
 
-cd ./defaults_provider/defaults_provider_annotations
-dart pub get
-cd ../../
+if [ "$arg" != "upgrade" ]
+then
+    echo "To perform an upgrade of all dependencies, run 'update-deps.sh upgrade'"
+fi
 
-cd ./defaults_provider/defaults_provider_generator
-dart pub get
-cd ../../
+function update()
+{
+    if [ "$2" = "upgrade" ]
+    then
+        echo "** Upgrading $1"
+        dart pub upgrade
+    else
+        echo "** Getting dependencies for $1"
+        dart pub get
+    fi
+}
 
-cd ./defaults_provider/defaults_provider_example
-dart pub get
-cd ../../
+for p in "${paths[@]}"
+do
+    echo $p
 
-
-cd ./defaults_provider/test
-dart pub get
-cd ../../
-
-# cd ./generators/validation_generator
-# dart pub get
-# cd ../../
-
-# cd ./generators/entity_adapter
-# dart pub get
-# cd ../../
-
-# cd ./generators/entity_adapter_generator
-# dart pub get
-# cd ../../
-
-cd ./arango_driver
-dart pub get
-cd ../
-
-cd ./security
-dart pub get
-cd ../
-
-cd ./nosql_repository
-dart pub get
-cd ../
-
-cd ./arango_repository
-dart pub get
-cd ../
+    (cd $p; update $p $arg )
+done
 
 
- 
