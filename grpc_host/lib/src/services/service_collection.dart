d5c45fa3682f902fc6e0e5efdce9a468934826f9
Rui Craveiro <ruicraveiro@squarealfa.com>
2021-04-13 11:12:13 +0100

    Removed repository creation from framework

--- grpc_host/lib/src/services/service_collection.dart
+++ grpc_host/lib/src/services/service_collection.dart
@@ -1,20 +1,10 @@
 import 'package:grpc/grpc.dart';
 import 'package:grpc_host/grpc_host.dart';
-import 'package:nosql_repository/nosql_repository.dart';
-import 'package:squarealfa_entity_adapter/squarealfa_entity_adapter.dart';
 
 abstract class ServiceCollection {
   final Container container;
 
   ServiceCollection(this.container);
-
-  EntityServicesParameters<TEntity> createServicesParameters<TEntity>(
-      EntityAdapter<TEntity> entityAdapter) {
-    final repository = container.resolve<Repository<TEntity>>();
-    final ret =
-        EntityServicesParameters.fromEntityAdapter(repository, entityAdapter);
-    return ret;
-  }
 
   List<Service> get services;
 }
